e8ce2b04e200f145763818ffa9eeb254dd1d0c05
LadyDammy <oreoluwadammie@gmail.com>
2022-11-28 16:18:41 -0800

    Concat edges

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/bin/usr/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+        language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[106:112] + str[:6] 
+print(str)
