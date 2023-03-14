fe359c97d60abd8d88a355a5d31e41bba539c04c
LadyDammy <oreoluwadammie@gmail.com>
2023-03-14 01:46:30 -0700

    Write a script

--- 0x12-javascript-warm_up/1-multi_languages.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const cString = 'C is fun';
-const pyString = 'Python is cool';
-const jsString = 'JavaScript is amazing';
-
-console.log(cString);
-console.log(pyString);
-console.log(jsString);
