9898c3f5029fefc42b41b858e676b8d68f0e689d
toshflux <tonnitizzo@gmail.com>
2023-03-31 13:36:51 +0300

    mod3

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# URL, sends a GET request to the URL, and displays the body of the response
-curl -s "$1" -X GET -L
+# Get the response body for a given URL for 200 status code responses.
+curl -sL "$1"
--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that sends a request to a URL passed as an argument
-curl -sI -w '%{response_code}' "$1" -o /dev/null
+# Sends a GET request to a given URL and display the response status code.
+curl -s -o /dev/null -w "%{http_code}" "$1"
